0afd01758d02da20a75823e2233048735d84eb7d
wangding <408542507@qq.com>
2019-03-17 20:03:11 +0800

    -all

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlhint: {
+      options: {
+                htmlhintrc: '.htmlhintrc'
+          
+      },
+        src: ['*.html']
+        
+    }
+                
+  });
+
+    grunt.loadNpmTasks('grunt-htmlhint');
+
+      grunt.registerTask('default', ['htmlhint']);
+
+};
---
+++ rectangle.js
@@ -0,0 +1,20 @@
+$(funtion() {
+    var $width = $('#width'),
+              $height = $('#height'),
+                    $btnCal = $('#calculate'),
+                          $perimeter = $('#perimeter'),
+                                $area = $('#area');
+
+    $btnCal.click(function(){
+          var w = Number($width.val()),
+              h = Number($height.val());
+
+        var p = 2 * (w + h),
+              a = w * h;
+
+        $perimeter.val(p);
+            $area.val(a);
+              
+    });
+
+});
