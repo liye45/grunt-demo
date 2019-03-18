d45ca44973b684dfbdc80ab365457f80bcf4f304
wangding <408542507@qq.com>
2019-03-18 16:00:11 +0800

    -all

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+      =    concat: {
+        js: {
+                  src: ['rectangle.js', 'calc.js'],
+            dest: 'dist/bundle.js'
+            
+        }
+                
+      }
+        
+    });
+
+      grunt.loadNpmTasks('grunt-contrib-concat');
+
+        grunt.registerTask('default', ['concat']);
+
+};
