dbf33ad58523519a5eb41e68228e495ddf497ec8
wangding <408542507@qq.com>
2019-03-18 10:09:46 +0800

    -a

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+                    
+module.exports = function (grunt) {
+  =  grunt.initConfig({
+    mocha: {
+      test: {
+                src: ['test/index.html'],
+        
+      },
+  options: {
+            run: true,
+          reporter: 'Dot'
+          
+  }
+      
+    }
+             
+  });
+
+    grunt.loadNpmTasks('grunt-mocha');
+
+      grunt.registerTask('default', ['mocha']);
+
+};
