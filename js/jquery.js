d33696ce4cee8f28daf60f9e80eca56f5eb8eab2
wangding <408542507@qq.com>
2019-03-19 12:23:12 +0800

    -all

---
+++ js/jquery.js
@@ -0,0 +1,49 @@
+(function( window, undefined  ) {
+
+  // Can't do this because several apps including ASP.NET trace
+  // // the stack via arguments.caller.callee and Firefox dies if
+  // // you try to trace through "use strict" call chains. (#13335)
+  // // Support: Firefox 18+
+  // //"use strict";
+  // var
+  //  // The deferred used on DOM ready
+  //    readyList,
+  //
+  //      // A central reference to the root jQuery(document)
+  //        rootjQuery,
+  //
+  //          // Support: IE<9
+  //            // For `typeof node.method` instead of `node.method !== undefined`
+  //              core_strundefined = typeof undefined,
+  //
+  //                // Use the correct document accordingly with window argument (sandbox)
+  //                  document = window.document,
+  //                    location = window.location,
+  //
+  //                      // Map over jQuery in case of overwrite
+  //                        _jQuery = window.jQuery,
+  //
+  //                          // Map over the $ in case of overwrite
+  //                            _$ = window.$,
+  //
+  //                              // [[Class]] -> type pairs
+  //                                class2type = {},
+  //
+  //                                  // List of deleted data cache ids, so we can reuse them
+  //                                    core_deletedIds = [],
+  //
+  //                                      core_version = "1.9.1",
+  //
+  //                                        // Save a reference to some core methods
+  //                                          core_concat = core_deletedIds.concat,
+  //                                            core_push = core_deletedIds.push,
+  //                                              core_slice = core_deletedIds.slice,
+  //                                                core_indexOf = core_deletedIds.indexOf,
+  //                                                  core_toString = class2type.toString,
+  //                                                    core_hasOwn = class2type.hasOwnProperty,
+  //                                                      core_trim = core_version.trim,
+  //
+  //                                                        // Define a local copy of jQuery
+  //                                                          jQuery = function( selector, context  ) {
+  //                                                          n
+})
